1b943ac6b3e99a739f5c1b89a2c0b89743709e89
Perla Godinez <contactoperlagodinez@gmail.com>
2025-04-03 13:50:46 -0600

    Update employee.component.ts

--- src/app/component/employee/employee.component.ts
+++ src/app/component/employee/employee.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card'
+import {MatButtonModule} from '@angular/material/button'
+
 
 @Component({
   selector: 'app-employee',
-  imports: [MatCardModule],
+  imports: [MatCardModule,MatButtonModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
